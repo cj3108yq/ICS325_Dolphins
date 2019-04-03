3d4bf650989b463f38e3f15ebfd7cb8a99255f84
t3eischens <qv9750cw@metrostate.edu>
2019-04-03 15:07:50 -0500

    Summary bug fix 3
    Fix team sql all cadences.

--- getArt_table.php
+++ getArt_table.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
   </head>
-<body>
+<body >
 
 <?php
     $q = trim($_GET['q']);
@@ -16,7 +17,7 @@
 ?>
 
 
-	 <table id="table_id" class="table_id" width="100%" style="width: 70%;">
+	 <table id="table_id" class="table_id" width="100%" style="width: 70%;" >
         <thead>
             <tr id="table-first-row">
                   <th>ART</th>
@@ -26,18 +27,20 @@
         <tbody>
         
              <?php
-                $sql = "SELECT t.team_id, c.total 
+                
+                $sql = "SELECT t.team_id, c.total
                   FROM capacity c RIGHT OUTER JOIN trains_and_teams t ON (t.team_id = c.team_id)
-                   WHERE (c.program_increment = '$q' OR c.total IS null) 
+                   WHERE ((c.program_increment = '$q' OR c.total IS null) OR (c.program_increment is null))
+                
                    AND t.team_id LIKE 'ART-%%%'
                     ORDER BY t.team_id";
-        
+
                  $result = $con->query($sql);
                     if($result ->num_rows > 0){
                         while($row = $result -> fetch_assoc()){
                          echo
                         "<tr>
-                             <td onclick = getAtTable('".$row["team_id"]."')>" .$row["team_id"] . "</td>
+                             <td onclick = getAtTable('".$row["team_id"]."','".$q."') onload = getAtTable('".$row["team_id"]."','".$q."')>".$row["team_id"]. "</td>
                              <td>" .((empty($row["total"])) ? 0 :$row["total"]) ."</td>
                          </tr>";
                         }
@@ -48,11 +51,29 @@
                  $result->close();
             ?>
         </tbody>
-     </table>
+        <tfoot>
+            <tr>
+                    <td> Total of Cadence</td>
+                <!--     <?php $sum = "SELECT SUM(c.total)
+                  FROM capacity c RIGHT OUTER JOIN trains_and_teams t ON (t.team_id = c.team_id)
+                   WHERE ((c.program_increment = '$q' OR c.total IS null) OR (c.program_increment is null))
+                   AND t.team_id LIKE 'ART-%%%' ORDER BY t.team_id";
+                    $result = mysqli_query($connection_mysql,$sql);
+                    $row = mysqli_fetch_array($result,MYSQLI_NUM);
+                        while($row=mysqli_fetch_array($sum))
+                            {
+                                $artSum = $row['sum(c.total)'];}
+                    ?>
+                    <td><?php echo $artSum ?></td> -->
+            </tr>
+        </tfoot>
+     </table >
     <script type="text/javascript">
          $(document).ready( function () {
          $('#table_id').DataTable();
              } );
     </script>
 </body>
+<footer >
+</footer>
 </html>
